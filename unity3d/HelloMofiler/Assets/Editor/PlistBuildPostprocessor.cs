589af955c892df707f632fc8d3e9c82ca8779d19
Bryan Tafel <bryantafel@Bryans-MBP.lan>
2017-05-19 17:33:11 -0400

    Add files

---
+++ unity3d/HelloMofiler/Assets/Editor/PlistBuildPostprocessor.cs
@@ -0,0 +1,30 @@
+﻿using UnityEditor.iOS.Xcode;
+using UnityEditor.Callbacks;
+using UnityEditor;
+using System.IO;
+
+
+	public class PlistBuildPostprocessor
+	{
+		[PostProcessBuild]
+		public static void OnPostprocessBuild(BuildTarget buildTarget, string path)
+		{
+			if (buildTarget == BuildTarget.iOS || buildTarget == BuildTarget.tvOS)
+			{
+				string projPath = PBXProject.GetPBXProjectPath(path);
+
+				PBXProject proj = new PBXProject();
+				proj.ReadFromString(File.ReadAllText(projPath));
+
+				string targetName = PBXProject.GetUnityTargetName();
+				string target = proj.TargetGuidByName(targetName);
+
+				//proj.AddFileToBuild(target, proj.AddFile("Plugins/Mofiler.framework", "Frameworks/Mofiler.framework", PBXSourceTree.Build));
+			proj.SetBuildProperty(target, "ENABLE_BITCODE", "YES");
+                proj.SetBuildProperty(target, "EMBEDDED_CONTENT_CONTAINS_SWIFT", "YES");
+
+				File.WriteAllText(projPath, proj.WriteToString());
+			}
+		}
+
+	}
