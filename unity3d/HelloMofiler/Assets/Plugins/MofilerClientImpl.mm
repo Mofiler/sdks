2d4739b886ba7703a0df29b07e633bd13eb43a10
Bryan Tafel <bryantafel@Bryans-MacBook-Pro.local>
2017-11-01 10:24:34 -0400

    Bump version iOS SDK 1.0.6

--- unity3d/HelloMofiler/Assets/Plugins/MofilerClientImpl.mm
+++ unity3d/HelloMofiler/Assets/Plugins/MofilerClientImpl.mm
@@ -44,7 +44,7 @@ NSString* appKey;
     
         mof.appKey = [NSString stringWithUTF8String:appKeyString];
     
-        [mof setSdkTypeAndVersionWithSdk_type: @"Unity iOS SDK" sdk_version:@"1.0.5.1"];
+        [mof setSdkTypeAndVersionWithSdk_type: @"Unity iOS SDK" sdk_version:@"1.0.6"];
     
         //TODO setUseLocation
         if(![mof.appName  isEqual: @""])
